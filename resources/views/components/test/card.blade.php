4acb718cec0bb709a076bc342b01930a5392eaae
fominvic81 <fomin.vic81@gmail.com>
2023-12-08 20:00:20 +0200

    Improve visuals of cards

--- resources/views/components/test/card.blade.php
+++ resources/views/components/test/card.blade.php
@@ -3,7 +3,7 @@
 <div class="bg-emerald-100 odd:bg-yellow-50 p-3 shadow-md rounded-md grid grid-cols-[auto_1fr_auto] gap-3">
     <x-common.image :src="App\Helpers\ImageHelper::url($test->image, URL::to('/images/img-placeholder.png'))"></x-common.image>
     <div>
-        <a class="text-lg font-semibold" href="{{ route('test.show', $test->id) }}">{{ $test->name }}</a>
+        <a class="text-xl font-semibold underline hover:text-yellow-600" href="{{ route('test.show', $test->id) }}">{{ $test->name }}</a>
         <div>
             <a href="{{ route('user.show', $test->user->id) }}" class="text-blue-600 hover:underline hover:text-blue-400 text-lg font-semibold">{{ $test->user->fullname }}</a>
         </div>
