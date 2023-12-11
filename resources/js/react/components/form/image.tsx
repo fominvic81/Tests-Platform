ccd4b987aaff64bba6f437e535b5da5a489167df
fominvic81 <fomin.vic81@gmail.com>
2023-12-11 12:32:07 +0200

    Add drop handler to form image component

--- resources/js/react/components/form/image.tsx
+++ resources/js/react/components/form/image.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import cn from 'classnames';
+import React, { useRef, useState } from 'react';
 
 
 interface Props {
@@ -10,12 +11,30 @@ interface Props {
 
 export const FormImage: React.FC<Props> = ({ name, nameDel, defaultSrc, onChange }) => {
     
+    const imageTypes = ['image/png', 'image/gif', 'image/jpeg', 'image/jpg'];
     const [image, setImage] = useState(defaultSrc);
+    const [dragOver, setDragOver] = useState(false);
+    const fileInput = useRef<HTMLInputElement>(null);
 
-    return <label className='w-full h-full border-2 border-dashed relative flex items-center justify-center'>
-        <img className='max-w-full max-h-full' src={ image ?? '/images/add-image.png' } alt='Зоображення' />
-        <div className='absolute w-full py-1 text-center bottom-0 bg-gray-200 bg-opacity-40 border-t'>{ image ? 'Видалити' : 'Додати зображення' }</div>
+    return <label
+        className={cn('w-full h-full border-2 border-dashed relative flex items-center justify-center bg-opacity-40 transition-all', dragOver && 'bg-gray-500 brightness-75')}
+        onDragEnter={() => setDragOver(true)}
+        onDragLeave={() => setDragOver(false)}
+        onDragOver={(event) => event.preventDefault()}
+        onDrop={(event) => {
+            event.preventDefault();
+            setDragOver(false);
+            const file = event.dataTransfer.files.item(0);
+            if (!file) return;
+            if (!imageTypes.includes(file.type)) return;
+            fileInput.current!.files = event.dataTransfer.files;
+            setImage(URL.createObjectURL(file));
+        }}
+    >
+        <img className='max-w-full max-h-full pointer-events-none' src={ image ?? '/images/add-image.png' } alt='Зоображення' />
+        <div className='absolute w-full py-1 text-center bottom-0 bg-gray-200 bg-opacity-40 border-t pointer-events-none'>{ image ? 'Видалити' : 'Додати зображення' }</div>
         <input
+            ref={ fileInput }
             type='file'
             name={ name }
             accept='image/*'
